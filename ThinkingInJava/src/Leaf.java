e6aed2430618029c71222330eb2ccdc7db6b9a55
junjunfly <180778754@qq.com>
2016-10-14 14:51:00 +0800

    construction test

---
+++ ThinkingInJava/src/Leaf.java
@@ -0,0 +1,17 @@
+/**
+ * Created by LuJun on 2016/10/13.
+ */
+public class Leaf {
+    int i = 0;
+    Leaf increment(){
+        i++;
+        return this;
+    }
+    void print(){
+        System.out.println("i = " + i);
+    }
+    public static void main (String args[]){
+        Leaf x = new Leaf();
+        x.increment().increment().increment().print();
+    }
+}
---
+++ ThinkingInJava/src/TestConstruct.java
@@ -0,0 +1,16 @@
+/**
+ * Created by LuJun on 2016/10/13.
+ */
+public class TestConstruct {
+    TestConstruct(){
+        this(23);
+    }
+    TestConstruct(int count){
+        System.out.println(count);
+    }
+
+    public  static void main(String args[]){
+        TestConstruct tc = new TestConstruct();
+    }
+
+}
