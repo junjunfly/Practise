337b441510e85fa786fe7ad972f9d9c483cde18e
junjunfly <180778754@qq.com>
2016-10-19 21:22:01 +0800

    构造器初始化顺序

---
+++ ThinkingInJava/src/Window.java
@@ -0,0 +1,32 @@
+/**
+ * Created by LuJun on 2016/10/19.
+ */
+public class Window {
+    Window(int marker) {
+        System.out.println("Window(" + marker + ")");
+    }
+}
+
+class House {
+    Window w1 = new Window(1);
+
+    House() {
+        System.out.println("House");
+        w3 = new Window(33);
+    }
+
+    Window w2 = new Window(2);
+    Window w3 = new Window(3);
+
+    void f() {
+        System.out.println("f()");
+    }
+}
+
+class OrderOfInitialization {
+    public static void main(String args[]) {
+        House h = new House();
+        h.f();
+    }
+}
+
